23e278cc4e22c619ff6f83010106bb3817401a56
ios52013 <2507233893@qq.com>
2016-12-16 11:21:05 +0800

    泛型方法，在返回值前面用尖括号标出泛型，前提是这个类型一定要是对象类型，而不能是int

---
+++ src/com/successful/chap06/sec04/Test.java
@@ -0,0 +1,31 @@
+package com.successful.chap06.sec04;
+
+/**
+ * 泛型方法
+ * 在返回值前面用尖括号括起来一个类型
+ * 要求：泛型方法的泛型 必须是一个对象类型
+ * @author ruiloveqi
+ *
+ */
+public class Test {
+
+	/**
+	 * 泛型方法
+	 * @param t
+	 */
+	//T必须是一个对象类型
+	public static <T> void func(T t){
+		System.out.println("T的类型是："+ t.getClass().getName());
+	}
+	
+	//主函数
+	public static void main(String[] args) {
+		//字符串
+		func("");
+		//
+		func(1);
+		func(3.14);
+		func(1.99f);
+		func(new Object());
+	}
+}
